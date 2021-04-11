8e7ad28cb80027ec1cb15f96daca3369796a40cd
WinterBlue16 <leekh090163@gmail.com>
2021-04-11 16:16:28 +0900

    .\Basic\18_오름차순, 내림차순 정렬하기.sql 생성

---
+++ Basic/18_오름차순, 내림차순 정렬하기.sql
@@ -0,0 +1,12 @@
+-- 예제 테이블 추가 시 테이블명 및 column명 변경
+
+-- 오름차순
+select [정렬할 column명]
+from [테이블명]
+order by [정렬 기준이 될 column명] asc;
+
+-- 내림차순
+select [정렬할 column명]
+from [테이블명]
+order by [정렬 기준이 될 column명] desc;
+
